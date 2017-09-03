75a1a2ca1b8525dca0e725b64ea8afdde5405f91
Nitish Puri <npuri1903@gmail.com>
2017-09-03 18:49:20 +0530

    feat: Add file for setting up model parameters.

---
+++ params.py
@@ -0,0 +1,12 @@
+PROJECT_PATH = '.'
+INPUT_PATH = PROJECT_PATH + '/input'
+
+METADATA_CSV_PATH = INPUT_PATH + '/metadata.csv'
+
+TRAIN_MASKS_CSV_PATH = INPUT_PATH + '/train_masks.csv'
+TRAIN_PATH = INPUT_PATH + '/train'
+TRAIN_MASKS_PATH = INPUT_PATH + '/train_masks'
+
+INPUT_SIZE = 128
+BATCH_SIZE = 16
+MAX_EPOCHS = 100
