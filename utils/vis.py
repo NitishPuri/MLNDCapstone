b5044f9f3ffca66e252e0eeb394a1656fff6f2e1
Nitish Puri <npuri1903@gmail.com>
2017-09-05 19:19:01 +0530

    feat: Add functions to visualize augmented dataset and curropted dataset.

--- utils/vis.py
+++ utils/vis.py
@@ -26,3 +26,74 @@ def vis_dataset(nrows = 5, ncols = 5, add_masks = False):
 #                 ax[i, j].imshow(mix, cmap = 'Greys_r', alpha = 0.6)
             counter += 1
     plt.show()
+
+
+# Sample some images from the dataset and show them in a grid
+def vis_augmented_dataset(nrows = 2, ncols = 2):
+    f, ax = plt.subplots(nrows = nrows, ncols = ncols, sharex = True, sharey = True, figsize=(20,20))
+    sampled_imgs = np.random.choice(train_masks['img'], nrows*ncols)
+#     sampled_imgs = [TRAIN_PATH + '/' + i for i in sampled_imgs]
+    
+    counter = 0
+    for i in range(nrows):
+        for j in range(ncols):
+            car_code, angle_code = filename_to_code(sampled_imgs[counter])
+            image = read_image(car_code, angle_code)
+            image = resize(image)
+
+            
+            mask = read_image(car_code, angle_code, True)
+            mask = resize(mask)
+            image = randomHueSaturationVariation(image, hue_shift_limit=(-50,50),
+                                                 sat_shift_limit=(-5, 5), val_shift_limit=(-15, 15))
+            image, mask = randomShiftScaleRotate(image, mask,  rotate_limit=(-5, 5))
+            image, mask = randomHorizontalFlip(image, mask)
+            image, mask = randomCrop(image, mask)
+
+            ax[i, j].imshow(image)
+            
+            #                 mix = cv2.bitwise_and(image, image, mask = mask)                    
+            ax[i, j].imshow(mask, alpha = 0.4)
+#                 ax[i, j].imshow(mix, cmap = 'Greys_r', alpha = 0.6)
+            counter += 1
+    plt.show()    
+
+
+# Sample some images from the dataset and show them in a grid
+def vis_curropted_dataset(add_masks = True):
+
+    curropted_masks =  ['0d1a9caf4350_14', '1e89e1af42e7_07', '2a4a8964ebf3_08', 
+                        '2ea62c1beee7_03', '2faf504842df_03', '2faf504842df_12' ]
+    # curropted_masks =  ['3afec4b5ac07_05', '3afec4b5ac07_12', '3afec4b5ac07_13', 
+    #                     '3afec4b5ac07_14', '3bca821c8c41_13', '4a4364d7fc6d_06' ] 
+    # curropted_masks =  ['4a4364d7fc6d_07', '4a4364d7fc6d_14', '4a4364d7fc6d_15', 
+    #                     '4baf50a3d8c2_05', '4e5ac4b9f074_11', '4f1f065d78ac_14' ]
+    # curropted_masks =  ['4f0397cf7937_05', '5df60cf7cab2_07', '5df60cf7cab2_15', 
+    #                     '6ba36af67cb0_07', '6bff9e10288e_01' ]
+
+    ncols = 2
+    nrows = int(np.ceil(len(curropted_masks)/ncols))
+#     print(ncols, nrows)
+    f, ax = plt.subplots(nrows = nrows, ncols = ncols, sharex = True, sharey = True, figsize=(20, 20))
+    sampled_imgs = np.random.choice(train_masks['img'], nrows*ncols)
+#     sampled_imgs = [TRAIN_PATH + '/' + i for i in sampled_imgs]
+    
+    counter = 0
+    for i in range(nrows):
+        for j in range(ncols):
+            if counter < len(curropted_masks):                            
+                car_code, angle_code = filename_to_code(curropted_masks[counter])
+                print (car_code, angle_code)
+                image = read_image(car_code, angle_code)
+                ax[i, j].imshow(image)
+
+                if add_masks:
+                    mask = read_image(car_code, angle_code, True)
+    #                 mix = cv2.bitwise_and(image, image, mask = mask)                    
+                    ax[i, j].imshow(mask, alpha = 0.9)
+                    ax[i, j].set_title(curropted_masks[counter])
+    #                 ax[i, j].imshow(mix, cmap = 'Greys_r', alpha = 0.6)
+                counter += 1
+    plt.show()
+    
+vis_curropted_dataset()    
