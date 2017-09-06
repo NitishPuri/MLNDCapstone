a6e43f5a79438933a70e4705903ecb7139f104d2
Nitish Puri <npuri1903@gmail.com>
2017-09-07 01:28:50 +0530

    feat: Add option to load test images using helper functions.

--- utils/filename.py
+++ utils/filename.py
@@ -7,11 +7,13 @@ def filename_to_code(filename):
 def code_to_filename(car_code, angle_code, mask = False):
     return car_code + '_' + angle_code + ('_mask.gif' if mask else '.jpg')
 
-def get_full_path(filename, mask = False):
+def get_full_path(filename, mask = False, test=False):
     if mask is True:
         return TRAIN_MASKS_PATH + '/' + filename
+    elif test is True:
+        return TEST_DIR_PATH + '/' + filename
     else :
         return TRAIN_PATH + '/' + filename
     
-def get_filepath_from_code(car_code, angle_code, mask = False):
-    return get_full_path(code_to_filename(car_code, angle_code, mask), mask)
+def get_filepath_from_code(car_code, angle_code, mask = False, test=False):
+    return get_full_path(code_to_filename(car_code, angle_code, mask), mask, test)
