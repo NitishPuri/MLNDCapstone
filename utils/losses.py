de2a00398fe87e0c6902a64300a846af1c2999b7
Nitish Puri <npuri1903@gmail.com>
2017-09-06 22:10:24 +0530

    feat: Fix dice_coeff for compatibility with other methods.

--- utils/losses.py
+++ utils/losses.py
@@ -14,19 +14,27 @@ def test(self):
 def dice(x, y):
     return 2*(len(set(x).intersection(set(y))))/(len(set(x)) + len(set(y)))    
 
-def dice_coeff(im1, im2, empty_score=1.0):
-    im1 = im1.astype(np.bool)
-    im2 = im2.astype(np.bool)
+# def dice_coeff(im1, im2, empty_score=1.0):
+#     im1 = im1.astype(np.bool)
+#     im2 = im2.astype(np.bool)
 
-    if im1.shape != im2.shape:
-        raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
+#     if im1.shape != im2.shape:
+#         raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
 
-    im_sum = im1.sum() + im2.sum()
-    if im_sum == 0:
-        return empty_score
+#     im_sum = im1.sum() + im2.sum()
+#     if im_sum == 0:
+#         return empty_score
 
-    intersection = np.logical_and(im1, im2)
-    return 2. * intersection.sum() / im_sum
+#     intersection = np.logical_and(im1, im2)
+#     return 2. * intersection.sum() / im_sum
+
+def dice_coeff(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return score
 
 def dice_loss(y_true, y_pred):
     return (1 - dice_coeff(y_true, y_pred))
