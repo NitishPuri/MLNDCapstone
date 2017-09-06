6229dca2adc0c55d653d3e246e9ab5b4da9ee4b9
Nitish Puri <npuri1903@gmail.com>
2017-09-07 01:31:21 +0530

    feat: Add more visualization functions
    * Visualize stats for manufaturer and baseline model.
    * Visualiz predictions from the baseline model.

--- utils/vis.py
+++ utils/vis.py
@@ -1,10 +1,15 @@
-from scipy import ndimage
-from matplotlib import pyplot as plt
-import seaborn as sns
 import cv2
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+from scipy import ndimage
 
+import utils.data as data
+from utils.filename import *
 from utils.params import *
+from utils.image import *
 
+train_masks = data.read_train_masks()
 
 # Sample some images from the dataset and show them in a grid
 def vis_dataset(nrows = 5, ncols = 5, add_masks = False):
@@ -96,4 +101,54 @@ def vis_curropted_dataset(add_masks = True):
                 counter += 1
     plt.show()
     
-vis_curropted_dataset()    
+def vis_manufacturer_distribution():
+    metadata = data.read_metadata()
+    metadata.index = metadata['id']
+
+    train_masks = data.read_train_masks()
+    train_ids = train_masks['img'].apply(lambda x: x[:-7])
+    train_ids = list(set(train_ids))
+    train_metadata = metadata.loc[train_ids]
+    plt.figure(figsize=(12, 10))
+    # train_metadata = 
+    sns.countplot(y="make", data=train_metadata)
+
+def plot_manufacturer_stats():
+    pd.read_csv("logs/man.csv")[['acc', 'loss', 'val_acc', 'val_loss']].plot()
+
+def plot_baseline_stats():
+    # baseline_history_DF = pd.DataFrame(baseline_history.history)
+    baseline_history_DF = pd.read_csv('logs/baseline.csv')
+    fig, axes = plt.subplots(nrows = 1, ncols = 2, figsize=(10, 5))
+    baseline_history_DF[['dice_coef', 'val_dice_coef']].plot(ax = axes[0]);
+    # pd.DataFrame(baseline_history.history)[['acc', 'val_acc']].plot()
+    baseline_history_DF[['loss', 'val_loss']].plot(ax = axes[1])
+
+def vis_baseline_predictions(baseline_model):
+    nrows = 3
+    f, ax = plt.subplots(nrows = nrows, ncols = 3, sharex = True, sharey = True, figsize=(20,20))
+    # print(ax.__class__)
+    sampled_imgs = np.random.choice(train_masks['img'], nrows)
+#     sampled_imgs = [TRAIN_PATH + '/' + i for i in sampled_imgs]
+    
+    counter = 0
+    for i in range(nrows):
+        # for j in range(ncols):
+        car_code, angle_code = filename_to_code(sampled_imgs[counter])
+        image = read_image(car_code, angle_code)
+        image = resize(image)
+
+        x_batch = []
+        x_batch.append(image)
+        x_batch = np.array(x_batch, np.float32) /255
+        pred = baseline_model.predict(x_batch).squeeze()
+
+        # print(image.__class__)
+        # print(ax.__class__)
+        ax[i, 0].imshow(image)
+        ax[i, 1].imshow(pred, cmap='gray')
+        ax[i, 2].imshow(pred > 0.001, cmap='gray')
+            
+        counter += 1
+
+    plt.show()    
