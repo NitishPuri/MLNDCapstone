80b61aad4298baae6bf1f76e732b852c33af1524
Nitish Puri <npuri1903@gmail.com>
2017-09-06 06:19:36 +0530

    feat: Update loss functions.

--- utils/losses.py
+++ utils/losses.py
@@ -5,19 +5,37 @@ from keras.losses import binary_crossentropy
 # Evaluation Metric: Dice Coefficient
 # Given two vector x and y, returns their dice distance
 
-laplace_smoothing = 0
+laplace_smoothing = 1
+
+def test(self):
+    print("helloooo")
+    pass
 
 def dice(x, y):
     return 2*(len(set(x).intersection(set(y))))/(len(set(x)) + len(set(y)))    
 
-def dice_coeff(y_true, y_pred):
-        y_true_f = K.flatten(y_true)
-        y_pred_f = K.flatten(y_pred)
-        intersection = K.sum(y_true_f*y_pred_f)
-        return (2. * intersection + laplace_smoothing)/(K.sum(y_true_f) + K.sum(y_pred_f) + 2*laplace_smoothing)
+def dice_coeff(im1, im2, empty_score=1.0):
+    im1 = im1.astype(np.bool)
+    im2 = im2.astype(np.bool)
+
+    if im1.shape != im2.shape:
+        raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
+
+    im_sum = im1.sum() + im2.sum()
+    if im_sum == 0:
+        return empty_score
+
+    intersection = np.logical_and(im1, im2)
+    return 2. * intersection.sum() / im_sum
 
 def dice_loss(y_true, y_pred):
     return (1 - dice_coeff(y_true, y_pred))
 
 def bce_dice_loss(y_true, y_pred):
     return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+
+def get_score(train_masks, avg_mask, thr):
+    d = 0.0
+    for i in range(train_masks.shape[0]):
+        d += dice_coeff(train_masks[i], avg_mask)
+    return d/train_masks.shape[0]
