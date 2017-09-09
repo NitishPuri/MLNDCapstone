ef5a1733c3c9469069cd6ec31be6db5d9d2d94ce
Nitish Puri <npuri1903@gmail.com>
2017-09-09 18:30:53 +0530

    feat: Add function to save layer activations and pretty display.

--- utils/read_activations.py
+++ utils/read_activations.py
@@ -38,6 +38,42 @@ def get_activations(model, model_inputs, print_shape_only=False, layer_name=None
             print(layer_activations)
     return activations
 
+def save_activations(model, model_inputs):
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    activation_maps = get_activations(model, model_inputs, print_shape_only = True)
+
+    batch_size = activation_maps[0].shape[0]
+    assert batch_size == 1, 'One image at a time to visualize.'
+
+    # activation_maps = activation_maps[0:5]
+
+    for layer_num, activation_map in enumerate(activation_maps):
+        layer_name = model.layers[layer_num].name
+        print('Displaying activation map {}, Layer {}'.format(layer_num, layer_name))
+        shape = activation_map.shape
+        activation_map = activation_map[0]
+        num_filters = shape[3]
+        fig_size = int(np.ceil(np.sqrt(num_filters)))
+
+        if(num_filters == 1):
+            img = activation_map[:,:,0]
+            fig, ax = plt.subplots(1, 1, figsize=(20, 20))
+            fig.suptitle('{} {}'.format(layer_num, layer_name))
+            ax.imshow(img)
+            fig.savefig('img/unet_128_{}_{}.png'.format(i, layer_name), dpi=fig.dpi)
+            plt.close(fig)
+        else:        
+            fig, ax = plt.subplots(fig_size, fig_size, figsize=(20, 20))
+            fig.suptitle('{} {}'.format(layer_num, layer_name))
+            for i in range(num_filters):
+                img = activation_map[:,:,i]
+                # ax[int(i/fig_size), (i%fig_size)].plot(img)
+                ax[int(i/fig_size), (i%fig_size)].imshow(img)
+            fig.savefig('img/unet_128_{}_{}.png'.format(layer_num, layer_name), dpi=fig.dpi)
+            plt.close(fig)
+
 
 def display_activations(activation_maps):
     import numpy as np
@@ -57,19 +93,34 @@ def display_activations(activation_maps):
     for i, activation_map in enumerate(activation_maps):
         print('Displaying activation map {}'.format(i))
         shape = activation_map.shape
-        if len(shape) == 4:
-            activations = np.hstack(np.transpose(activation_map[0], (2, 0, 1)))
-        elif len(shape) == 2:
-            # try to make it square as much as possible. we can skip some activations.
-            activations = activation_map[0]
-            num_activations = len(activations)
-            if num_activations > 1024:  # too hard to display it on the screen.
-                square_param = int(np.floor(np.sqrt(num_activations)))
-                activations = activations[0: square_param * square_param]
-                activations = np.reshape(activations, (square_param, square_param))
-            else:
-                activations = np.expand_dims(activations, axis=0)
-        else:
-            raise Exception('len(shape) = 3 has not been implemented.')
-        plt.imshow(activations, interpolation='None', cmap='jet')
-        plt.show()
+        activation_map = activation_map[0]
+        num_filters = shape[3]
+        fig_size = int(np.ceil(np.sqrt(num_filters)))
+
+        if(num_filters == 1):
+            img = activation_map[:,:,0]
+            fig, ax = plt.subplots(1, 1, figsize=(20, 20))
+            ax.imshow(img)
+        else:        
+            fig, ax = plt.subplots(fig_size, fig_size, figsize=(20, 20))
+            for i in range(num_filters):
+                img = activation_map[:,:,i]
+                ax[int(i/fig_size), (i%fig_size)].imshow(img)
+
+        # if len(shape) == 4:
+        #     # activations = np.hstack(np.transpose(activation_map[0], (2, 0, 1)))
+        #     num_filters = shape[3]
+        # elif len(shape) == 2:
+        #     # try to make it square as much as possible. we can skip some activations.
+        #     activations = activation_map[0]
+        #     num_activations = len(activations)
+        #     if num_activations > 1024:  # too hard to display it on the screen.
+        #         square_param = int(np.floor(np.sqrt(num_activations)))
+        #         activations = activations[0: square_param * square_param]
+        #         activations = np.reshape(activations, (square_param, square_param))
+        #     else:
+        #         activations = np.expand_dims(activations, axis=0)
+        # else:
+        #     raise Exception('len(shape) = 3 has not been implemented.')
+        # plt.imshow(activations, interpolation='None', cmap='jet')
+        # plt.show()
