bce9896986913baab29139e0d6109a36aaf5a314
Nitish Puri <npuri1903@gmail.com>
2017-09-10 18:35:11 +0530

    chore: Refactor `vis_dataset` methods.

--- utils/vis.py
+++ utils/vis.py
@@ -8,97 +8,74 @@ import utils.data as data
 from utils.filename import *
 from utils.params import *
 from utils.image import *
-
-from vis.visualization import visualize_activation, get_num_filters
+from utils.preprocess import *
 
 train_masks = data.read_train_masks()
 
-# Sample some images from the dataset and show them in a grid
-def vis_dataset(nrows = 5, ncols = 5, add_masks = False):
+def vis_dataset(nrows = 5, ncols = 5, mask_alpha = 0.4, augment = False):
+    
+    """ Sample some images from the dataset and show them in a grid."""
+
     f, ax = plt.subplots(nrows = nrows, ncols = ncols, sharex = True, sharey = True, figsize=(20,20))
     sampled_imgs = np.random.choice(train_masks['img'], nrows*ncols)
-#     sampled_imgs = [TRAIN_PATH + '/' + i for i in sampled_imgs]
     
     counter = 0
     for i in range(nrows):
         for j in range(ncols):
             car_code, angle_code = filename_to_code(sampled_imgs[counter])
             image = read_image(car_code, angle_code)
+            mask = read_image(car_code, angle_code, True)
+
+            if augment is True:
+                image = resize(image)
+                mask = resize(mask)
+                image = randomHueSaturationVariation(image, hue_shift_limit=(-50,50),
+                                                    sat_shift_limit=(-5, 5), val_shift_limit=(-15, 15))
+                image, mask = randomShiftScaleRotate(image, mask,  rotate_limit=(-5, 5))
+                image, mask = randomHorizontalFlip(image, mask)
+                
             ax[i, j].imshow(image)
-            
-            if add_masks:
-                mask = read_image(car_code, angle_code, True)
-#                 mix = cv2.bitwise_and(image, image, mask = mask)                    
-                ax[i, j].imshow(mask, alpha = 0.4)
-#                 ax[i, j].imshow(mix, cmap = 'Greys_r', alpha = 0.6)
+            ax[i, j].imshow(mask, alpha = mask_alpha)
+
             counter += 1
     plt.show()
 
 
 # Sample some images from the dataset and show them in a grid
-def vis_augmented_dataset(nrows = 2, ncols = 2):
-    f, ax = plt.subplots(nrows = nrows, ncols = ncols, sharex = True, sharey = True, figsize=(20,20))
-    sampled_imgs = np.random.choice(train_masks['img'], nrows*ncols)
-#     sampled_imgs = [TRAIN_PATH + '/' + i for i in sampled_imgs]
-    
-    counter = 0
-    for i in range(nrows):
-        for j in range(ncols):
-            car_code, angle_code = filename_to_code(sampled_imgs[counter])
-            image = read_image(car_code, angle_code)
-            image = resize(image)
+def vis_curropted_dataset():
 
-            
-            mask = read_image(car_code, angle_code, True)
-            mask = resize(mask)
-            image = randomHueSaturationVariation(image, hue_shift_limit=(-50,50),
-                                                 sat_shift_limit=(-5, 5), val_shift_limit=(-15, 15))
-            image, mask = randomShiftScaleRotate(image, mask,  rotate_limit=(-5, 5))
-            image, mask = randomHorizontalFlip(image, mask)
-            image, mask = randomCrop(image, mask)
-
-            ax[i, j].imshow(image)
-            
-            #                 mix = cv2.bitwise_and(image, image, mask = mask)                    
-            ax[i, j].imshow(mask, alpha = 0.4)
-#                 ax[i, j].imshow(mix, cmap = 'Greys_r', alpha = 0.6)
-            counter += 1
-    plt.show()    
-
-
-# Sample some images from the dataset and show them in a grid
-def vis_curropted_dataset(add_masks = True):
+    """ List of incorrectly labeled images is given."""
+    """ Sample and show some images from it."""
 
     curropted_masks =  ['0d1a9caf4350_14', '1e89e1af42e7_07', '2a4a8964ebf3_08', 
-                        '2ea62c1beee7_03', '2faf504842df_03', '2faf504842df_12' ]
-    # curropted_masks =  ['3afec4b5ac07_05', '3afec4b5ac07_12', '3afec4b5ac07_13', 
-    #                     '3afec4b5ac07_14', '3bca821c8c41_13', '4a4364d7fc6d_06' ] 
-    # curropted_masks =  ['4a4364d7fc6d_07', '4a4364d7fc6d_14', '4a4364d7fc6d_15', 
-    #                     '4baf50a3d8c2_05', '4e5ac4b9f074_11', '4f1f065d78ac_14' ]
-    # curropted_masks =  ['4f0397cf7937_05', '5df60cf7cab2_07', '5df60cf7cab2_15', 
-    #                     '6ba36af67cb0_07', '6bff9e10288e_01' ]
+                        '2ea62c1beee7_03', '2faf504842df_03', '2faf504842df_12', 
+                        '3afec4b5ac07_05', '3afec4b5ac07_12', '3afec4b5ac07_13', 
+                        '3afec4b5ac07_14', '3bca821c8c41_13', '4a4364d7fc6d_06',
+                        '4a4364d7fc6d_07', '4a4364d7fc6d_14', '4a4364d7fc6d_15', 
+                        '4baf50a3d8c2_05', '4e5ac4b9f074_11', '4f1f065d78ac_14',
+                        '4f0397cf7937_05', '5df60cf7cab2_07', '5df60cf7cab2_15', 
+                        '6ba36af67cb0_07', '6bff9e10288e_01' ]
 
     ncols = 2
-    nrows = int(np.ceil(len(curropted_masks)/ncols))
-#     print(ncols, nrows)
+    nrows = 2
+
+    curropted_masks = np.random.choice(curropted_masks, ncols*nrows)
     f, ax = plt.subplots(nrows = nrows, ncols = ncols, sharex = True, sharey = True, figsize=(20, 20))
     sampled_imgs = np.random.choice(train_masks['img'], nrows*ncols)
-#     sampled_imgs = [TRAIN_PATH + '/' + i for i in sampled_imgs]
     
     counter = 0
     for i in range(nrows):
         for j in range(ncols):
             if counter < len(curropted_masks):                            
                 car_code, angle_code = filename_to_code(curropted_masks[counter])
-                print (car_code, angle_code)
+                # print (car_code, angle_code)
                 image = read_image(car_code, angle_code)
                 ax[i, j].imshow(image)
 
-                if add_masks:
-                    mask = read_image(car_code, angle_code, True)
-    #                 mix = cv2.bitwise_and(image, image, mask = mask)                    
-                    ax[i, j].imshow(mask, alpha = 0.9)
-                    ax[i, j].set_title(curropted_masks[counter])
+                mask = read_image(car_code, angle_code, True)
+#                 mix = cv2.bitwise_and(image, image, mask = mask)                    
+                ax[i, j].imshow(mask, alpha = 0.9)
+                ax[i, j].set_title(curropted_masks[counter])
     #                 ax[i, j].imshow(mix, cmap = 'Greys_r', alpha = 0.6)
                 counter += 1
     plt.show()
@@ -114,6 +91,7 @@ def vis_manufacturer_distribution():
     plt.figure(figsize=(12, 10))
     # train_metadata = 
     sns.countplot(y="make", data=train_metadata)
+    plt.show()
 
 def plot_manufacturer_stats():
     pd.read_csv("logs/man.csv")[['acc', 'loss', 'val_acc', 'val_loss']].plot()
@@ -160,6 +138,9 @@ def vis_predictions(model, fullRes=False):
 
 ## http://nbviewer.jupyter.org/github/raghakot/keras-vis/blob/master/examples/vggnet/activation_maximization.ipynb
 def vis_activation_maximizations(model, layer_idx):
+    
+    from vis.visualization import visualize_activation, get_num_filters
+
     num_filters = get_num_filters(model.layers[layer_idx])
     print("num_filters : ", num_filters)
     fig_size = int(np.ceil(np.sqrt(num_filters)))
