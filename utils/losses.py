717e622506dc991a852e1c2ea8edd80f1424cb8a
Nitish Puri <npuri1903@gmail.com>
2017-09-11 23:07:20 +0530

    feat: Add numpy based dice implementation due to data type errors.

--- utils/losses.py
+++ utils/losses.py
@@ -12,19 +12,19 @@ from keras.losses import binary_crossentropy
 def dice(x, y):
     return 2*(len(set(x).intersection(set(y))))/(len(set(x)) + len(set(y)))    
 
-# def dice_coeff(im1, im2, empty_score=1.0):
-#     im1 = im1.astype(np.bool)
-#     im2 = im2.astype(np.bool)
+def dice_coeff_numpy(im1, im2, empty_score=1.0):
+    im1 = im1.astype(np.bool)
+    im2 = im2.astype(np.bool)
 
-#     if im1.shape != im2.shape:
-#         raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
+    if im1.shape != im2.shape:
+        raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
 
-#     im_sum = im1.sum() + im2.sum()
-#     if im_sum == 0:
-#         return empty_score
+    im_sum = im1.sum() + im2.sum()
+    if im_sum == 0:
+        return empty_score
 
-#     intersection = np.logical_and(im1, im2)
-#     return 2. * intersection.sum() / im_sum
+    intersection = np.logical_and(im1, im2)
+    return 2. * intersection.sum() / im_sum
 
 def dice_coeff(y_true, y_pred):
     smooth = 1.
